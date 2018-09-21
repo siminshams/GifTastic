06beb00d941900f1bc2dda65975b1c7d3b0aac3a
Simin Shamsfallah <Simin.Shamsfallah@Earlens.com>
2018-09-21 01:23:11 -0700

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     var animals = ["cat", "bird", "dog", "goose", "dolphin", "squirrel", "rabbit", "chicken", "goldfish", "tigger", "frog"];
 
     function renderButton() {
@@ -12,7 +13,7 @@ $(document).ready(function () {
             btn.attr("data-name", animals[i]);
             btn.text(animals[i]);
             $("#animal-view").append(btn);
-
+            console.log(btn);
         }
     }
 
@@ -24,16 +25,17 @@ $(document).ready(function () {
         renderButton();
 
     });
+
     renderButton();
 
     var apiKey = "Xqec8EZ0nSj3wtzf4iWBvGG4FJ2VwdiS";
 
-    $("button").on("click", function () {
+    $(document).on("click", ".animal", function () {
         var btn = $(this).attr("data-name");
         console.log(btn);
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + btn + "&api_key=" + apiKey + "&limit=10";
 
-        // Perfoming an AJAX GET request to our queryURL
+
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -41,19 +43,38 @@ $(document).ready(function () {
 
             .then(function (response) {
                 console.log(response);
-             var result = response.data;
-             
-             for (var i = 0; i< result.length; i++){
-                 var gifDiv = $("<div>");
-                 var rating = result[i].rating;
-                 var p = $("<p>").text("Rating: " + rating);
-                 var image = $("<img>");
-                 image.attr("src", result[i].images.fixed_height.url);
-                 gifDiv.append(p);
-                 gifDiv.append(image);
-                 $("#images").prepend(gifDiv);
-             }
-             
+                var result = response.data;
+
+                for (var i = 0; i < result.length; i++) {
+                    var gifDiv = $("<div>");
+                    var rating = result[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var image = $("<img>");
+
+                    image.attr("src", result[i].images.fixed_height_still.url);
+                    image.attr("data-state", "still");
+                    image.attr("data-still", result[i].images.fixed_height_still.url);
+                    image.attr("data-animate", result[i].images.fixed_height.url)
+                    gifDiv.append(p);
+                    gifDiv.append(image);
+                    $("#images").prepend(gifDiv);
+                    state();
+                }
+                function state() {
+                    $("img").click(function () {
+
+                        var state = $(this).attr("data-state");
+
+                        if (state == "still") {
+                            $(this).attr("src", $(this).data("animate"));
+                            $(this).attr("data-state", "animate");
+                        }
+                        else {
+                            $(this).attr("src", $(this).data("still"));
+                            $(this).attr("data-state", "still");
+                        }
+                    })
+                }
 
 
             });
